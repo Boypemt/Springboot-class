30fc6abc8a8f43f585f720f0869d4adfa82aaaf6
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2026-07-16 15:48:07 +0700

    update to use Get Mapping

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,13 +1,15 @@
 package th.mfu;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     String hello() {
         return "Hello World!";
     }
